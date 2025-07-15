338640b7a0000b3baaf6a13c7eda3332d32aa57a
Anthonny Rios <anthonnyrios12@gmail.com>
2025-07-15 14:33:36 -0300

    Primeros pasos en Java

---
+++ PrimerosPasos/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola soy dekin");
+
+        int edad = 24;
+        double estatura = 1.67;
+        boolean tieneCarnet = false;
+        char letra = 'T';
+        String direccion = "Direccion Falsa 123";
+        long numeroLargo= 12342312;
+
+        System.out.println("Mi edad es: "+ edad);
+        System.out.println("Mi estatura es: "+ estatura);
+        System.out.println("Mi direccion es: "+ direccion);
+    }
+}
