45ffaecf44409e7170184462f63800d03e0b8030
Anthonny Rios <anthonnyrios12@gmail.com>
2025-07-18 14:37:13 -0300

    Practica condicional If

--- Ejercicioif/src/Main.java
+++ Ejercicioif/src/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        /*
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Ingrese su edad: ");
@@ -15,5 +17,26 @@ public class Main {
         else{
             System.out.println("Sos menor de 18, ingreso rechazado!");
         }
+
+    */
+
+    int edad;
+
+    Scanner ingreso = new Scanner(System.in);
+    System.out.println("Ingrese la edad del alumno: ");
+    edad = ingreso.nextInt();
+
+    if(edad >= 4 && edad <= 6 ){
+        System.out.println("Salon Kinder: Lunes y Miercoles de 16 a 17hs");
+    } else if (edad <= 8) {
+        System.out.println("Salon 1er año: Martes y Jueves de 16:30 a 17:30hs");
+    } else if (edad <= 10) {
+        System.out.println("Salon 2do año: Martes y Jueves de 17:30 a 19hs");
+    } else if (edad <= 13) {
+        System.out.println("Salon 3er año: Lunes y Miercoles de 17 a 18:30hs");
+    } else {
+        System.out.println("La edad ingresada es incorrecta! vuelva a intentarlo.");
+    }
+
     }
 }
